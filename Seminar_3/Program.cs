68675ee02d2ef1673d5bcd824dfe8871c3e94dc1
Andrey <andreypodmoskov@gmail.com>
2023-08-03 02:51:34 +0300

    Отредактировали лекцию 3

--- Seminar_3/Program.cs
+++ Seminar_3/Program.cs
@@ -1,103 +1,128 @@
-﻿// Напишите программу, которая выводит случайное число из
-// отрезка [10, 99] и показывает наибольшую цифру числа.
+﻿// Задача №17. Напишите программу, которая 
+// принимает на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти
+// плоскости, в которой находится эта точка.
  
-// Напирмер:
-// 78 -> 8
-// 12 -> 2
-// 85 -> 8
+// Console.WriteLine("Введите число");
+// int X = Convert.ToInt32(Console.ReadLine());
  
-// int num = new Random().Next(10,100);
-// Console.WriteLine(num);
+// Console.WriteLine("Введите число");
+// int Y = Convert.ToInt32(Console.ReadLine());
  
-// int num1= num / 10;
-// int num2= num % 10;
- 
-// if(num1>num2)
+// if(X>0 && Y>0)
 // {
-// Console.WriteLine(num1);
+// Console.WriteLine("1");
 // }
-// else if(num1==num2)
+// else if(X<0 && Y>0)
 // {
-// Console.WriteLine("Числа равны");
+// Console.WriteLine("2");
 // }
-// else
+// else if(X<0 && Y<0)
 // {
-// Console.WriteLine(num2);
+// Console.WriteLine("3");
 // }
- 
-// Напишите программу, которая выводит случайное 
-// трехзначное число и удаляет вторую цифру этого числа.
- 
-// int num = new Random().Next(100,1000);
-// Console.WriteLine(num);
- 
-// int num1= num / 100;
-// int num2= num % 10;
-// int result= num1 * 10 + num2;
- 
-// Console.WriteLine(result);
- 
-// Напишите программу, которая будет принимать на вход
-// два числа и выводить, является ли второе число кратным
-// первому. Если число 2 не кратно числу 1, то программа 
-// выводит остаток от деления.
- 
-// 34, 5 -> не кратно, остаток 4
-// 16, 4 -> кратно
- 
-// Console.WriteLine("Введите число:");
-// int num1 = Convert.ToInt32(Console.ReadLine());
- 
-// Console.WriteLine("Введите число:");
-// int num2 = Convert.ToInt32(Console.ReadLine());
- 
-// if(num1 % num2==0)
+// else if(X>0 && Y<0)
 // {
-// Console.WriteLine("Кратные");
+// Console.WriteLine("4");
 // }
 // else
 // {
-// Console.WriteLine("Остаток равен " + num1 % num2);
+// Console.WriteLine("Точка лежит на оси");
 // }
  
-// Напишите программу, которая принимает на вход число и
-// ипроверяет, кратно ли оно одновременно 7 и 23.
+// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
  
-// 14 -> нет
-// 46 -> нет
-// 161 -> да
- 
-// Console.WriteLine("Введите число:");
-// int num1 = Convert.ToInt32(Console.ReadLine());
- 
-// if((num1 % 7==0)&&(num1 % 23==0))
+// Console.Write("Введите номер четверти: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if(num == 1)
 // {
-// Console.WriteLine("Да");
+// Console.WriteLine("X и Y больше 0");
+// }
+// else if (num == 2)
+// {
+// Console.WriteLine("X меньше 0, а Y больше 0");
+// }
+// else if (num == 3)
+// {
+// Console.WriteLine("X и Y меньше 0");
+// }
+// else if (num == 4)
+// {
+// Console.WriteLine("X больше 0, а Y меньше 0");
 // }
 // else
 // {
-// Console.WriteLine("Нет");
+// Console.WriteLine("Пожалуйста введите номер четверти от 1 до 4");
 // }
  
-// напишите программу, которая принимает на вход два числа и
-// проверяет, является ли одно число квадратом другого.
+// Напишите программу, которая по заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
  
-// 5, 25 -> да
-// -4, 16 -> да
-// 25, 5 -> да
-// 8, 9 -> нет
+// Console.Write("Введите число : ");
+// int num =Convert.ToInt32(Console.ReadLine());
  
-// Console.WriteLine("Введите число:");
-// int num1 = Convert.ToInt32(Console.ReadLine());
- 
-// Console.WriteLine("Введите число:");
-// int num2 = Convert.ToInt32(Console.ReadLine());
- 
-// if((num1== num2*num2)||(num2== num1*num1))
+// switch(num)
 // {
-// Console.WriteLine("Да");
-// }
-// else
+// case 1:
 // {
-// Console.WriteLine("Нет");
+// Console.Write("X>0 Y>0");
+// break;
 // }
+// case 2:
+// {
+// Console.Write("X<0 Y>0");
+// break;
+// }
+// case 3:
+// {
+// Console.Write("X<0 Y<0");
+// break;
+// }
+// case 4:
+// {
+// Console.Write("X>0 Y<0");
+// break;
+// }
+// default:
+// {
+// Console.Write("Не подходит");
+// break;
+// }
+// }
+ 
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+ 
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+ 
+// Console.Write("Введите число : ");
+// int X1 =Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число : ");
+// int Y1 =Convert.ToInt32(Console.ReadLine());
+ 
+// Console.Write("Введите число : ");
+// int X2 =Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число : ");
+// int Y2 =Convert.ToInt32(Console.ReadLine());
+ 
+// double s=Math.Sqrt((Math.Pow((X2-X1),2) + Math.Pow((Y2-Y1),2)));
+ 
+// System.Console.WriteLine(s);
+ 
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+// от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+ 
+// Console.Write("Введите число: ");
+// int num =Convert.ToInt32(Console.ReadLine());
+ 
+// for(int i =1; i<=num; i++)
+// {
+// Console.WriteLine(Math.Pow(i,2));
+// }
+ 
+// int day=2;
+// Console.WriteLine("Day =" + day +"\r\nЭто не выходной!");
+// int day=2;
+// Console.WriteLine("Day =" + day +"\nЭто не выходной!");
