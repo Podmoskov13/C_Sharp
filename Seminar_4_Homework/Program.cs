51d8c1ec50edaf9eea8476f24dbf1c45bd59bc6a
Andrey <andreypodmoskov@gmail.com>
2023-08-03 12:59:16 +0300

    Решили первую задачу в 4 домашнем задании

--- Seminar_4_Homework/Program.cs
+++ Seminar_4_Homework/Program.cs
@@ -2,6 +2,21 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+// Console.WriteLine("Введите число A");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число B");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// int GetNumDegree(int a,int b)
+// {
+//     int result = 1;
+//     for(int i = 0; i < b; i++)
+//     {
+//         result = result * a;
+//     }
+//     return result;
+// }
+// Console.WriteLine(GetNumDegree(a, b));
 
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
@@ -9,6 +24,8 @@
 // 82 -> 10
 // 9012 -> 12
 
+// Console.WriteLine("Введите число ");
+// int a = Convert.ToInt32(Console.ReadLine());
 
 
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
