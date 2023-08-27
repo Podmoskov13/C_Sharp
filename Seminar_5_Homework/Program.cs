a6ea192b8a057f6408979f1d638470da5ff4aa24
Andrey <andreypodmoskov@gmail.com>
2023-08-27 23:45:33 +0300

    Домашнее задание по 5 уроку, решили задачу 38

--- Seminar_5_Homework/Program.cs
+++ Seminar_5_Homework/Program.cs
@@ -45,4 +45,17 @@
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
 
+// System.Console.Write("Введите количество элементов в массиве: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[num];
+// int count = 0;
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(0, 100);
+//     {
+//         count += array[i];
+//     }
+// }
+// System.Console.WriteLine("");
+// System.Console.WriteLine(count);
 
